f033a41363e2ae8124a00630c841e09ca99a871d
Swarna7414 <swarnasaisankar044@gmail.com>
2025-06-26 00:24:31 -0500

    modified The Home page

--- src/Pages/Develop.tsx
+++ src/Pages/Develop.tsx
@@ -8,13 +8,16 @@ const Develop: React.FC = () => {
     <section id="home" className="hero">
       <div className="container">
         <div className="hero-content">
-          <h1 className="hero-title">
-            Hello, I'm  <br /><span className='text-black hover:text-blue-500'>Debesh Jha <span className='wave-on-hover'>👋</span></span>
+          <h1 className="hero-title group-hover-wrapper">
+            Hello, I'm <br />
+            <span className="name-hover-group">
+            Debesh Jha <span className="wave-on-hover">👋</span>
+            </span>
           </h1>
           <h2 className="hero-subtitle">AI Researcher & Medical Imaging Scientist</h2>
           <p className="hero-description">
-            I'm an Assistant Professor and AI Scientist developing human-centered, trustworthy clinical AI for medical imaging—advancing 
-            diagnosis through multimodal learning, dataset creation, and deployable healthcare solutions.
+            I'm an Assistant Professor and AI Scientist developing human-centered, trustworthy clinical AI for medical imaging-advancing diagnosis through multimodal learning,
+             dataset creation, real-world deployment, and clinician-aligned innovation.
           </p>
           <div className="hero-buttons">
             <button className="btn btn-primary">
