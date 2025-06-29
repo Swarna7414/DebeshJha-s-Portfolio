d0b5b8bede49b73e45fa21f63e98fdd2cbc75156
Swarna7414 <swarnasaisankar044@gmail.com>
2025-06-28 22:44:16 -0500

    modified h1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import type { Config } from "tailwindcss";
+import scrollbarHide from "tailwind-scrollbar-hide";
 
 const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -25,7 +26,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    scrollbarHide
+  ],
 };
 
 export default config;
